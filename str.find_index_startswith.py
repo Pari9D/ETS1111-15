55d2f452764d3805ffbaad01103dbd1beb20d5e6
Pari9D <parahem.asfaw@aastustudent.edu.et>
2025-03-25 13:28:00 +0300

    sting of find index and startswith
    here I've continued to practice more with strings daily to keep up with the class

---
+++ str.find_index_startswith.py
@@ -0,0 +1,9 @@
+brown='the qUCIck bRoWn fOx JUMPES oVEr thE LazY DOG'
+blue='tHe SKy is Blue'
+print(brown.find('s'))
+print(blue.find('tHe'))
+print(brown.index('W'))
+print(blue.index('i'))
+print(brown.startswith('fOx'))
+print(blue.startswith('the'))
+print(blue.startswith('tHe'))
