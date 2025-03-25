c22330f78de03efeb91d6f248bc3e5fcd541e34c
Pari9D <parahem.asfaw@aastustudent.edu.et>
2025-03-25 16:53:44 +0300

    string of endswith_count_replace
    I'm still practicing to catch up with the other students

---
+++ st.endswith_count_replace.py
@@ -0,0 +1,8 @@
+brown='the qUCIck bRoWn fOx JUMPES oVEr thE LazY DOG'
+blue='tHe SKy is Blue'
+print(brown.endswith('dog'))
+print(brown.endswith('DOG'))
+print(blue.count('e'))
+print('aaaaaa'.count('aa'))
+print(blue.replace('SKy','ocean'))
+print('JUMPES' in brown)
