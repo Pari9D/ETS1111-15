af4dd0517aee2bbd378f3b97922ea6b74b1b334c
Pari9D <parahem.asfaw@aastustudent.edu.et>
2025-03-25 12:00:46 +0300

    capitalise_title_swapcase
    Here are another 3 strings we can use

---
+++ str.title_capitalise_swapcase.py
@@ -0,0 +1,5 @@
+brown='the qUCIck bRoWn fOx JUMPES oVEr thE LazY DOG'
+blue='tHe SKy is Blue'
+print(brown.title()+"|"+blue.capitalize())
+print(brown.capitalize()+"|"+blue.swapcase())
+print(brown.swapcase()+"|"+blue.title())
