4fb935f41d684a8300f1084e2af62787d5daec17
Pari9D <parahem.asfaw@aastustudent.edu.et>
2025-03-27 17:58:08 +0300

    string of strip_lstip_rstip
    The strip sting removes leading (left) and trailing (right) spaces (or specified characters) from a string. And returns: A new string with the spaces/characters removed
    
    When It's lstrp, It removes only the right side and lstip only the left side

---
+++ str.strip_lstrip_rstrip.py
@@ -0,0 +1,6 @@
+brown='...the qUCIck bRoWn fOx JUMPES oVEr thE LazY DOG...'
+blue='   tHe SKy is Blue   '
+print(blue.strip(  ))
+print(blue.strip(   ))
+print(brown.lstrip('.' ))
+print(brown.strip('...'))
