928a8d4ab9497a3ffea5115b97ca17042f9b90fa
Pari9D <parahem.asfaw@aastustudent.edu.et>
2025-03-27 19:00:14 +0300

    string of split join isalpha
    the string split splits the words while join joins them and isalpha cheeks whether the characters are alphanumeric or not

---
+++ str.split_join_isalpha.py
@@ -0,0 +1,8 @@
+brown='the qUCIck bRoWn fOx JUMPES oVEr thE LazY DOG'
+blue='tHe SKy is Blue'
+green='.'
+print(brown.split())
+print(green.join(brown))
+print(' -|- '.join([brown,blue,green]))
+print(all(word.isalpha() for word in [brown, blue, green]))
+print(brown.isalpha)
