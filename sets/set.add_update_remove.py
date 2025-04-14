51a6a3edd84c9b2b2715616e235defb72d85f3ab
Pari9D <parahem.asfaw@aastustudent.edu.et>
2025-04-14 09:40:51 +0300

    set operation with add update and remove
    While the operation .add adds specific variables to our set, .update updates the whole set with multiple new variables, and .remove deletes a particular variable from our set

---
+++ sets/set.add_update_remove.py
@@ -0,0 +1,15 @@
+fruits = {"apple", "banana", "cherry"}
+fruits.add("orange")
+
+print('\n',fruits,'\n')
+
+more_fruits = ["grape", "mango"]
+fruits.update(more_fruits)
+
+print(fruits,'\n')
+
+fruits.remove("banana")
+
+print(fruits,'\n')
+
+
