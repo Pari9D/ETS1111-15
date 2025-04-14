7d6d4e025850de6b141f74fa138cd567c99f821b
Pari9D <parahem.asfaw@aastustudent.edu.et>
2025-04-14 09:43:56 +0300

    Update tup_creation.py

---
+++ Tuple/tup_creation.py
@@ -0,0 +1,17 @@
+fruits_list = ['apple', 'banana', 'cherry']
+fruits_tuple = tuple(fruits_list)
+
+print("\nList:", fruits_list)
+print("Tuple:", fruits_tuple,'\n')
+
+word = "Python"
+word_tuple = tuple(word)
+
+print("String:", word)
+print("Tuple:", word_tuple,'\n')
+
+numbers = [1, 2, 3, 4]
+numbers_tuple = tuple(numbers)
+
+print(numbers_tuple,'\n')
+
