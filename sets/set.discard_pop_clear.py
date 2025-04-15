e10b0ceddd14524bfc2b26ee6137208c7736d3c5
Pari9D <parahem.asfaw@aastustudent.edu.et>
2025-04-15 07:45:49 +0300

    set operation with discard, pop, and, clear
    discard: Removes a specific item from the set without raising an error if the item doesn’t exist.
    pop: Removes and returns an arbitrary element from the set (because sets are unordered).
    clear: Removes all elements from the set.

---
+++ sets/set.discard_pop_clear.py
@@ -0,0 +1,13 @@
+Fruits = {"apple", "banana", "cherry","watermelon","Wine"}
+Fruits.discard("banana")  
+Fruits.discard("mango") 
+
+print('\n',Fruits,'\n')
+
+Fruits.pop()
+
+print('Removed',Fruits,'\n')
+
+Fruits.clear()
+
+print(Fruits,'\n')
